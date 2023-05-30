8570f366ed8e861546da800da0501756aaaaaaab
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-05-30 10:21:54 -0400

    edit product

--- models/product.js
+++ models/product.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const p = path.join(
-	path.dirname(require.main.filename),
+	path.dirname(process.mainModule.filename),
 	"data",
 	"products.json"
 );
@@ -18,7 +18,8 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-	constructor(title, imageUrl, price, description) {
+	constructor(id, title, imageUrl, price, description) {
+		this.id = id;
 		this.title = title;
 		this.imageUrl = imageUrl;
 		this.price = price;
@@ -26,12 +27,23 @@ module.exports = class Product {
 	}
 
 	save() {
-		this.id = Math.random().toString();
 		getProductsFromFile((products) => {
-			products.push(this);
-			fs.writeFile(p, JSON.stringify(products), (err) => {
-				console.log(err);
-			});
+			if (this.id) {
+				const existingProductIndex = products.findIndex(
+					(prod) => prod.id === this.id
+				);
+				const updatedProducts = [...products];
+				updatedProducts[existingProductIndex] = this;
+				fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+					console.log(err);
+				});
+			} else {
+				this.id = Math.random().toString();
+				products.push(this);
+				fs.writeFile(p, JSON.stringify(products), (err) => {
+					console.log(err);
+				});
+			}
 		});
 	}
 
@@ -41,7 +53,7 @@ module.exports = class Product {
 
 	static findById(id, cb) {
 		getProductsFromFile((products) => {
-			const product = products.find((prod) => prod.id === id);
+			const product = products.find((p) => p.id === id);
 			cb(product);
 		});
 	}
