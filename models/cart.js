594f71bc659215e4b36572a3273035cf7199e2df
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-05-30 14:30:20 -0400

    deleting cart items

--- models/cart.js
+++ models/cart.js
@@ -1,11 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const p = path.join(
-	path.dirname(process.mainModule.filename),
-	"data",
-	"cart.json"
-);
+const p = path.join(path.dirname(require.main.filename), "data", "cart.json");
 
 module.exports = class Cart {
 	static addProduct(id, productPrice) {
@@ -33,6 +29,26 @@ module.exports = class Cart {
 			}
 			cart.totalPrice = cart.totalPrice + +productPrice;
 			fs.writeFile(p, JSON.stringify(cart), (err) => {
+				console.log(err);
+			});
+		});
+	}
+
+	static deleteProduct(id, productPrice) {
+		fs.readFile(p, (err, fileContent) => {
+			if (err) {
+				return;
+			}
+			const updatedCart = { ...JSON.parse(fileContent) };
+			const product = updatedCart.products.find((prod) => prod.id === id);
+			const productQty = product.qty;
+			updatedCart.products = updatedCart.products.filter(
+				(prod) => prod.id !== id
+			);
+			updatedCart.totalPrice =
+				updatedCart.totalPrice - productPrice * productQty;
+
+			fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
 				console.log(err);
 			});
 		});
