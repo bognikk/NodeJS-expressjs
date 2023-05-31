8544be910d5c8ca18b9370b755c2459b8f581f52
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-05-31 10:21:15 -0400

    sequelize

--- util/database.example.js
+++ util/database.example.js
@@ -1,10 +1,13 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
-	host: "localhost",
-	user: "root",
-	database: "node-complete",
-	password: "HERE GOES THE PASSWORD",
-});
+const sequelize = new Sequelize(
+	"node-complete",
+	"root",
+	"HERE GOES THE PASSWORD",
+	{
+		dialect: "mysql",
+		host: "localhost",
+	}
+);
 
-module.exports = pool.promise();
+module.exports = sequelize;
