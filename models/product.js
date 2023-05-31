6370df32c734fd321464287300354e10fcda87e4
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-05-31 10:26:45 -0400

    defining a mode

--- models/product.js
+++ models/product.js
@@ -1,30 +1,26 @@
-const db = require("../util/database");
+const Sequelize = require("sequelize");
+const sequelize = require("../util/database");
 
-const Cart = require("./cart");
+const Product = sequelize.define("product", {
+	id: {
+		type: Sequelize.INTEGER,
+		autoIncrement: true,
+		allowNull: false,
+		primatyKey: true,
+	},
+	title: Sequelize.STRING,
+	price: {
+		type: Sequelize.DOUBLE,
+		allowNull: false,
+	},
+	imageUrl: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
+	description: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
+});
 
-module.exports = class Product {
-	constructor(id, title, imageUrl, price, description) {
-		this.id = id;
-		this.title = title;
-		this.imageUrl = imageUrl;
-		this.price = price;
-		this.description = description;
-	}
-
-	save() {
-		return db.execute(
-			"INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)",
-			[this.title, this.price, this.description, this.imageUrl]
-		);
-	}
-
-	static deleteById(id) {}
-
-	static fetchAll() {
-		return db.execute("SELECT * FROM products");
-	}
-
-	static findById(id) {
-		return db.execute("SELECT * FROM products WHERE products.id = ?", [id]);
-	}
-};
+module.exports = Product;
