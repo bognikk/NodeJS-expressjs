f9cb6956386e5cd893ace3139d70256846021eab
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-06-01 11:58:55 -0400

    adding existing products and retrieving cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,23 +10,23 @@ exports.getProducts = (req, res, next) => {
 				path: "/products",
 			});
 		})
-		.catch((err) => console.log(err));
+		.catch((err) => {
+			console.log(err);
+		});
 };
 
 exports.getProduct = (req, res, next) => {
 	const prodId = req.params.productId;
-
 	// Product.findAll({ where: { id: prodId } })
-	// 	.then((products) => {
-	// 		res.render("shop/product-detail", {
-	// 			product: products[0],
-	// 			pageTitle: products[0].title,
-	// 			path: "/products",
-	// 		});
-	// 	})
-	// 	.catch((err) => console.log(err));asdfasdfasdfasdfasdfasdfasdfasdfasdfasdf
-
-	Product.findByPk(prodId)
+	//   .then(products => {
+	//     res.render('shop/product-detail', {
+	//       product: products[0],
+	//       pageTitle: products[0].title,
+	//       path: '/products'
+	//     });
+	//   })
+	//   .catch(err => console.log(err));
+	Product.findById(prodId)
 		.then((product) => {
 			res.render("shop/product-detail", {
 				product: product,
@@ -46,7 +46,9 @@ exports.getIndex = (req, res, next) => {
 				path: "/",
 			});
 		})
-		.catch((err) => console.log(err));
+		.catch((err) => {
+			console.log(err);
+		});
 };
 
 exports.getCart = (req, res, next) => {
@@ -70,31 +72,30 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
 	const prodId = req.body.productId;
 	let fetchedCart;
-
+	let newQuantity = 1;
 	req.user
 		.getCart()
 		.then((cart) => {
 			fetchedCart = cart;
-			return cart
-				.getProducts({ where: { id: prodId } })
-				.then((products) => {
-					let product;
-					if (products.length > 0) {
-						product = products[0];
-					}
-					let newQuantity;
-					if (product) {
-						// ...
-					}
-					return Product.findByPk(prodId)
-						.then((product) => {
-							return fetchedCart.addProduct(product, {
-								through: { quantity: newQuantity },
-							});
-						})
-						.catch((err) => console.log(err));
-				})
-				.catch((err) => console.log(err));
+			return cart.getProducts({ where: { id: prodId } });
+		})
+		.then((products) => {
+			let product;
+			if (products.length > 0) {
+				product = products[0];
+			}
+
+			if (product) {
+				const oldQuantity = product.cartItem.quantity;
+				newQuantity = oldQuantity + 1;
+				return product;
+			}
+			return Product.findById(prodId);
+		})
+		.then((product) => {
+			return fetchedCart.addProduct(product, {
+				through: { quantity: newQuantity },
+			});
 		})
 		.then(() => {
 			res.redirect("/cart");
