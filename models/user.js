c72d91d4e5e2d6e795e05e2c4e73dcd1089678a0
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-06-05 10:03:34 -0400

    add to cart functionality

--- models/user.js
+++ models/user.js
@@ -17,7 +17,9 @@ class User {
 	}
 
 	addToCart(product) {
-		const updatedCart = { items: [{ ...product, quantity: 1 }] };
+		const updatedCart = {
+			items: [{ productId: new ObjectId(product._id), quantity: 1 }],
+		};
 		const db = getDb();
 		return db
 			.collection("user")
