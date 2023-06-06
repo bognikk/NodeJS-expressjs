0410a633b8b0cdb3d2292c4428933d113ea6df73
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-06-06 18:23:56 -0400

    Getting Orders

--- models/user.js
+++ models/user.js
@@ -104,21 +104,13 @@ class User {
 			});
 	}
 
-	// getOrders() {
-	// 	const db = getDb();
-	// 	return db
-	// 		.collection("orders")
-	// 		.insertOne(this.cart)
-	// 		.then((result) => {
-	// 			this.cart = { items: [] };
-	// 			return db
-	// 				.collection("users")
-	// 				.updateOne(
-	// 					{ _id: new ObjectId(this._id) },
-	// 					{ $set: { cart: { items: [] } } }
-	// 				);
-	// 		});
-	// }
+	getOrders() {
+		const db = getDb();
+		return db
+			.collection("orders")
+			.find({ "user._id": new ObjectId(this._id) })
+			.toArray();
+	}
 
 	static findById(userId) {
 		const db = getDb();
