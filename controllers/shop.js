1f8934bf74be5e86e78d70891f0e2812bdec83fa
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-06-07 11:24:55 -0400

    loading the cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -41,10 +41,12 @@ exports.getIndex = (req, res, next) => {
 		});
 };
 
-exports.getCart = (req, res, next) => {
-	req.user
-		.getCart()
-		.then((products) => {
+exports.getCart = async (req, res, next) => {
+	await req.user
+		.populate("cart.items.productId")
+		.then((user) => {
+			console.log(user.cart.items);
+			const products = user.cart.items;
 			res.render("shop/cart", {
 				path: "/cart",
 				pageTitle: "Your Cart",
