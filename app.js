71e335ee2043675c566ee781b35d682bb00c5aa3
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-06-12 09:13:14 -0400

    using csrf

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
 
 const errorController = require("./controllers/error");
 const User = require("./models/user");
@@ -19,6 +20,8 @@ const store = new MongoDBStore({
 	uri: MONGODB_URI,
 	collection: "sessions",
 });
+
+const csrfProtection = csrf();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -38,6 +41,8 @@ app.use(
 	})
 );
 
+app.use(csrfProtection);
+
 app.use((req, res, next) => {
 	if (!req.session.user) {
 		return next();
@@ -50,6 +55,12 @@ app.use((req, res, next) => {
 		.catch((err) => {
 			console.log(err);
 		});
+});
+
+app.use((req, res, next) => {
+	res.locals.isAuthenticated = req.session.isLoggedIn;
+	res.locals.csrfToken = req.csrfToken();
+	next();
 });
 
 app.use("/admin", adminRoutes);
