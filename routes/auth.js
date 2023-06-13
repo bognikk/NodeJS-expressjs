3140249525ec2afde9804c017ea4cac9fef05014
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-06-13 11:35:37 -0400

    built-In and custom validators

--- routes/auth.js
+++ routes/auth.js
@@ -11,7 +11,15 @@ router.post("/logout", authController.postLogout);
 router.get("/signup", authController.getSignup);
 router.post(
 	"/signup",
-	check("email").isEmail().withMessage("Please enter a valid email."),
+	check("email")
+		.isEmail()
+		.withMessage("Please enter a valid email.")
+		.custom((value, { req }) => {
+			if (value === "test@test.com") {
+				throw new Error("This email address if forbidden.");
+			}
+			return true;
+		}),
 	authController.postSignup
 );
 router.get("/reset", authController.getReset);
