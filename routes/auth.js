317a9e56cf6f2e7a7eaf5e29d902b398b56903f3
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-06-13 11:30:45 -0400

    using validation error messages

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { check } = require("express-validator");
 
 const authController = require("../controllers/auth");
 
@@ -8,7 +9,11 @@ router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
 router.post("/logout", authController.postLogout);
 router.get("/signup", authController.getSignup);
-router.post("/signup", authController.postSignup);
+router.post(
+	"/signup",
+	check("email").isEmail().withMessage("Please enter a valid email."),
+	authController.postSignup
+);
 router.get("/reset", authController.getReset);
 router.post("/reset", authController.postReset);
 router.get("/reset/:token", authController.getNewPassword);
